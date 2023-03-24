6f488aa24f04d978ba574228290a402c25a5375a
saepulfariz <saepulfariz108@gmail.com>
2023-03-24 16:43:25 +0700

    Upload file

---
+++ README.md
@@ -0,0 +1,6 @@
+# notes-app-back-end
+## npm install
+## npm run start
+
+notes-app-back-end Dicoding
+http://notesapp-v1.dicodingacademy.com/
---
+++ src/notes.js
@@ -0,0 +1,11 @@
+const notes = [{
+    title: 'ksks',
+    tags: 'lalla',
+    body: 'kaka',
+    id: 'R3WAow6KqnRfjxfT',
+    createdAt: '2023-03-15T03:45:54.593Z',
+    updatedAt: '2023-03-15T03:45:54.593Z'
+}]
+module.exports = {
+    notes
+};
